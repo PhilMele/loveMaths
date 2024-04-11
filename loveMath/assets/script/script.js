1cf4db17facc3f166f7af3fce79fc0717a564238
PhilMele <phil@gomyprocurement.com>
2024-04-11 13:37:45 +0100

    Add code for addition question can be displayed to user

--- loveMath/assets/script/script.js
+++ loveMath/assets/script/script.js
@@ -1,5 +1,6 @@
 console.log("connected!")
 
+//starts at the moment the page is loaded
 document.addEventListener("DOMContentLoaded", function(){
     let buttons = document.getElementsByTagName("button");
 
@@ -9,22 +10,34 @@ document.addEventListener("DOMContentLoaded", function(){
                     alert("You clicked submit!")
                 }else{
                     let gameType = this.getAttribute("data-type");
-                    alert(`You clicked! ${gameType}`)
+                    //when a data-type button is click that is not submit
+                    //rungame function starts based on gameType clicked
+                    runGame(gameType)
                 
                 }
             
             })
         }
+        //looks for game-type addition to 
+        //start this action
+        runGame("addition");
 
 })
 /**
     *The main game loop, called when the script is first loaded 
     *and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
     //generate random numbers
     let num1 = Math.floor(Math.random()*25 +1);
     let num2 = Math.floor(Math.random()*25 +1);
+
+    if (gameType === 'addition'){
+        displayAdditionQuestion(num1, num2)
+    } else{
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknwon game type: ${gameType}. Aborting`;
+    }
 
 }
 
@@ -37,7 +50,12 @@ function incrementScore(){}
 
 function incrementWrongAnswer(){}
 
-function displayAdditionQuestion(){}
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+"
+
+}
 
 function displaySubtractQuestion(){}
 

e0bdd3e13feadc0878159c412018088989cdc3e8
PhilMele <phil@gomyprocurement.com>
2024-04-11 13:10:00 +0100

    Add addEventListener with alerts triggered when buttons are cliked

--- loveMath/assets/script/script.js
+++ loveMath/assets/script/script.js
@@ -1,3 +1,23 @@
+console.log("connected!")
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+        for (let button of buttons){
+            button.addEventListener("click", function(){
+                if (this.getAttribute("data-type") === "submit"){
+                    alert("You clicked submit!")
+                }else{
+                    let gameType = this.getAttribute("data-type");
+                    alert(`You clicked! ${gameType}`)
+                
+                }
+            
+            })
+        }
+
+})
+
 function runGame(){}
 
 function checkAnswer(){}

c598f57ac1cf7488f4b4fbb7234bebc8fa308c84
PhilMele <phil@gomyprocurement.com>
2024-04-11 12:50:23 +0100

    Add functions to script.js

--- loveMath/assets/script/script.js
+++ loveMath/assets/script/script.js
@@ -1,1 +1,15 @@
-console.log("connnected")
+function runGame(){}
+
+function checkAnswer(){}
+
+function calculateCorrectAnswer(){}
+
+function incrementScore(){}
+
+function incrementWrongAnswer(){}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractQuestion(){}
+
+function displayMultiplyQuestion(){}

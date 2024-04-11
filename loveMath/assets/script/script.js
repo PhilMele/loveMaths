8adfeff3cc9cabe47d2a4143c92db66f8fd11154
PhilMele <phil@gomyprocurement.com>
2024-04-11 14:54:52 +0100

    Add multiply and substract functions

--- loveMath/assets/script/script.js
+++ loveMath/assets/script/script.js
@@ -21,6 +21,9 @@ document.addEventListener("DOMContentLoaded", function(){
         //looks for game-type addition to 
         //start this action
         runGame("addition");
+        runGame("subtract");
+        runGame("multiply");
+        runGame("division");
 
 })
 /**
@@ -33,11 +36,23 @@ function runGame(gameType){
     let num2 = Math.floor(Math.random()*25 +1);
 
     if (gameType === 'addition'){
-        displayAdditionQuestion(num1, num2)
-    } else{
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknwon game type: ${gameType}. Aborting`;
-    }
+            displayAdditionQuestion(num1, num2)
+        } 
+        else if(gameType === 'subtract'){
+            displaySubtractQuestion(num1, num2)
+
+        }
+        else if(gameType === 'multiply'){
+            displayMultiplyQuestion(num1, num2)
+
+        }
+        else if(gameType === 'division'){
+            displayDivisionQuestion(num1, num2)
+        }
+        else{
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknwon game type: ${gameType}. Aborting`;
+        }
 
 }
 
@@ -80,6 +95,13 @@ function calculateCorrectAnswer(){
 
     if (operator === "+"){
         return [operand1 + operand2, "addition"]
+    }else if(operator === "-"){
+        return[operand1 - operand2, "subtract"]
+
+    }else if(operator === "*"){
+        return[operand1 * operand2, "multiply"]
+    }else if(operator === "%"){
+        return[operand1 % operand2, "division"]
     }else{
         alert(`Unimplemented operator ${operator}`)
         throw (`Unimplement operator: ${operator}. Aborting.`)
@@ -113,6 +135,23 @@ function displayAdditionQuestion(operand1, operand2){
 
 }
 
-function displaySubtractQuestion(){}
+function displaySubtractQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "-"
 
-function displayMultiplyQuestion(){}
+}
+
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "*"
+
+}
+
+function displayDivisionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = "%"
+
+}
